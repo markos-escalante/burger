f9ba12e1d60a92cb47928eed07ae4896fe71c63f
Markos Rafael <escalante.markos18@gmail.com>
2021-01-17 20:34:11 -0800

    Placed the first values being inserted into burger_name column into a string.

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,10 +1,10 @@
 -- INITIAL THREE ENTRIES
 
 INSERT INTO burgers (burger_name, devoured)
-VALUES (Baconator, 1);
+VALUES ("Baconator", 1);
 
 INSERT INTO burgers (burger_name, devoured)
-VALUES (Ultimate Egg Burger, 1);
+VALUES ("Ultimate Egg Burger", 1);
 
 INSERT INTO burgers (burger_name, devoured)
-VALUES (Korean BBQ style Burger, 1);
+VALUES ("Korean BBQ style Burger", 1);
