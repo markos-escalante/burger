deeccbe3f30f1f5dcf368c7943cf628d88c060de
Markos Rafael <escalante.markos18@gmail.com>
2021-01-17 20:48:37 -0800

    Change var to const.

--- config/orm.js
+++ config/orm.js
@@ -1,6 +1,6 @@
 const connection = require("./connection.js");
 
-var orm = {
+const orm = {
   selectAll: function(tableInput, colToSearch, valOfCol) {
     var queryString = "SELECT * FROM ?? WHERE ?? = ?";
     connection.query(queryString, [tableInput, colToSearch, valOfCol], function(err, result) {
