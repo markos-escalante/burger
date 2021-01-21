667f34fd5f162a5faca83f8feb181a66017fddc3
Markos Rafael <escalante.markos18@gmail.com>
2021-01-21 14:47:50 -0800

    Set up our server

--- server.js
+++ server.js
@@ -1,2 +1,22 @@
 const express = require("express");
 const exphbs = require("express-handlebars");
+
+const PORT = proces.env.PORT || 3000;
+
+const app = express();
+
+app.use(express.static("public"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+const routes = require("./controllers/burgers_controllers.js");
+
+app.use(routes);
+
+app.listen(PORT, function() {
+    console.log("App now listening at localhost: " + PORT);
+});
