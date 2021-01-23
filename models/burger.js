fe9912be87c5562f4a89e9772e5174addc56a4f3
Markos Rafael <escalante.markos18@gmail.com>
2021-01-22 18:56:36 -0800

    create burger object and export.

--- models/burger.js
+++ models/burger.js
@@ -1,6 +1,22 @@
-const ormMethods = require("./config/orm.js");
+const orm = require("../config/orm.js");
+
+const burger = {
+    selectAll: function (cb) {
+        orm.selectAll("burgers", function (res) {
+            cb(res);
+        });
+    },
+    insertOne: function (colName, vals, cb) {
+        orm.insertOne("burgers", colName, vals, function (res) {
+            cb(res);
+        });
+    },
+    updateOne: function (objColVals, condtion, cb) {
+        orm.updateOne("burgers", objColVals, condition, function (res) {
+            cb(res);
+        });
+    }
+};
 
 
-
-
-module.exports = ormMethods;
+module.exports = burger;
